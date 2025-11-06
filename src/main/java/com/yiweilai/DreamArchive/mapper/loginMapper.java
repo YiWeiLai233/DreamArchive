a00157d2dad27d2a8d51cfed7fd57468b952c4f7
YiweiLai233 <859399899@qq.com>
2025-11-07 03:01:45 +0800

    未完成

--- src/main/java/com/yiweilai/DreamArchive/mapper/loginMapper.java
+++ src/main/java/com/yiweilai/DreamArchive/mapper/loginMapper.java
@@ -2,6 +2,6 @@ package com.yiweilai.DreamArchive.mapper;
 
 import com.yiweilai.DreamArchive.DTO.User;
 
-public class loginMapper {
-//    User login(User user);
+public interface loginMapper {
+    User login(User user);
 }
